0ab168e2dcdaffbe0fbe7cab86cdf9605f385627
Akshay-source <akshaytanwar26@gmail.com>
2024-09-05 06:42:19 +0000

    made changes in scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+
+sudo npm install forever -g
+
 cd /home/ec2-user/nodejs-todo-app/
 npm install
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-todo-app/
-node index.js
+forever start index.js
 
